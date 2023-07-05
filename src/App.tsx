aa122ca7a77c93a62685f43dcca7287cea296b3d
brigss <brigitta.rucz@yahoo.com>
2023-07-05 18:00:40 +0300

    Add base

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Container from "./components/container/Container";
+import "./assets/stylesheets/main.css";
+
+function App() {
+  return (
+    <div>
+      <Container />
+    </div>
+  );
+}
+
+export default App;

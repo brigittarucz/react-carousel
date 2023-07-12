5581d2ad91621d594fe4b15083c5cde26b1d7bf7
brigss <brigitta.rucz@yahoo.com>
2023-07-12 11:53:51 +0300

    Add Carousel.tsx

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,31 +1,77 @@
-const Carousel = () => {
-  return (
-    <div className="main">
-      <section className="main_section-left">
-        <div className="main_image-preview main_image-preview--left">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          quod autem, debitis at repudiandae nam dolores magni dolore odio nisi
-          repellat voluptatem voluptate soluta? Odit ipsum ab quo possimus ullam
-          quisquam eos vel necessitatibus aspernatur placeat quam et in,
-          voluptatibus non, expedita molestias. Ab commodi totam, tempore
-          architecto quam rerum?
-        </div>
-      </section>
-      <section className="main_section-middle">
-        <div className="main_image-spotlight">box</div>
-      </section>
-      <section className="main_section-right">
-        <div className="main_image-preview main_image-preview--right">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit enim iure
-          obcaecati nesciunt doloribus ipsum voluptates laudantium, itaque
-          eligendi maxime, quis necessitatibus in! Ullam nemo autem unde
-          similique ipsa adipisci animi, cumque perspiciatis, quo quam
-          voluptatum praesentium neque vero minus! Aliquid at ipsam totam maxime
-          repudiandae rerum qui quaerat nihil?
-        </div>
-      </section>
-    </div>
+import { useEffect, useState } from "react";
+import generateCarouselSkeleton from "components/utils/generateCarouselSkeleton";
+
+const Carousel = ({
+  imageNumber,
+  imagesCarousel,
+}: {
+  imageNumber: number;
+  imagesCarousel: string[];
+}) => {
+  const [carousel, setCarousel] = useState(
+    generateCarouselSkeleton(null, null, null)
   );
+
+  useEffect(() => {
+    switch (imagesCarousel.length) {
+      case 0:
+        setCarousel(<div>No images</div>);
+        break;
+      case 1:
+        setCarousel(
+          generateCarouselSkeleton(null, imagesCarousel[imageNumber], null)
+        );
+        break;
+      case 2:
+        if (imageNumber === imagesCarousel.length - 1) {
+          setCarousel(
+            generateCarouselSkeleton(
+              imagesCarousel[imageNumber],
+              imagesCarousel[0],
+              null
+            )
+          );
+        } else {
+          setCarousel(
+            generateCarouselSkeleton(
+              imagesCarousel[imageNumber],
+              imagesCarousel[imageNumber + 1],
+              null
+            )
+          );
+        }
+        break;
+      default:
+        if (imageNumber === imagesCarousel.length - 1) {
+          setCarousel(
+            generateCarouselSkeleton(
+              imagesCarousel[imageNumber],
+              imagesCarousel[0],
+              imagesCarousel[1]
+            )
+          );
+        } else if (imageNumber === imagesCarousel.length - 2) {
+          setCarousel(
+            generateCarouselSkeleton(
+              imagesCarousel[imageNumber],
+              imagesCarousel[imageNumber + 1],
+              imagesCarousel[0]
+            )
+          );
+        } else {
+          setCarousel(
+            generateCarouselSkeleton(
+              imagesCarousel[imageNumber],
+              imagesCarousel[imageNumber + 1],
+              imagesCarousel[imageNumber + 2]
+            )
+          );
+        }
+        break;
+    }
+  }, [imageNumber, imagesCarousel]);
+
+  return <div className="main">{carousel}</div>;
 };
 
 export default Carousel;

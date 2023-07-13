d3e3a93f65560fe8c150600c8d1d3266614408a3
brigss <brigitta.rucz@yahoo.com>
2023-07-13 09:41:30 +0300

    Add placeholder commented out images for testing

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,14 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App images={[]} />
+    <App
+      images={
+        [
+          // <img src="https://media4.giphy.com/media/HDaBrYcECrDzR8OBmh/giphy.gif?cid=ecf05e471345wf16qp2wt0ed7lztzismwe3lyn979d1monjp&ep=v1_gifs_search&rid=giphy.gif&ct=g" />,
+          // <img src="https://media4.giphy.com/media/3o7abJxL1w984uW2AM/giphy.gif?cid=ecf05e471345wf16qp2wt0ed7lztzismwe3lyn979d1monjp&ep=v1_gifs_search&rid=giphy.gif&ct=g" />,
+          // <img src="https://media2.giphy.com/media/xTka00xv2CNa6FzFaU/giphy.gif?cid=ecf05e471345wf16qp2wt0ed7lztzismwe3lyn979d1monjp&ep=v1_gifs_search&rid=giphy.gif&ct=g" />,
+        ]
+      }
+    />
   </React.StrictMode>
 );
